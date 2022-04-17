c7b33f187b8972bf40138d51f91365eda040ca7d
xandowski <xandy-moraes@hotmail.com>
2022-04-17 10:32:37 -0300

    tuple and readonly array

---
+++ src/006-tuple/tuple.ts
@@ -0,0 +1,8 @@
+// Tuple
+const clientData: readonly [number, string] = [1, 'Alexandre']
+const clientData2: readonly [number, string, string?] = [1, 'Alexandre']
+const clientData3: readonly [number, string, ...string[]] = [1, 'Alexandre']
+
+// readonly array
+const array1: readonly string[] = ['Alexandre', 'Morais']
+const array2: ReadonlyArray<string> = ['Alexandre', 'Morais']
